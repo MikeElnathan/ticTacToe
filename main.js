fa7d68a8e45f1c0cbd87aa890a1aa46187642d0a
MikeElnathan <mikeson.anthony@gmail.com>
2024-12-26 09:14:23 +0800

    player can draw on boaard

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 const mainBoard = document.getElementById("board");
 const makeBoard = document.getElementById("createBoard");
 console.log("   hello");
-// ------------------------------------------------
+// --------------------------------------------------------------
 function createboard(){
-    
-    for (let i = 0; i < 9; i++ ){
-        mainBoard.value = '';
-        const squareBoard = document.createElement("div");
-        squareBoard.addEventListener("click", ()=>{
-            console.log("A sauare is pressed");
-        })
+        mainBoard.innerHTML = '';
 
-        squareBoard.className = "square";
-        mainBoard.appendChild(squareBoard);
+        for (let i = 0; i < 9; i++ ){
+            const squareBoard = document.createElement("div");
+
+            squareBoard.id = `${i}`;
+
+            squareBoard.addEventListener("click", ()=>{
+                console.log(`A square numbered ${squareBoard.id} is pressed`);
+                gameStart(squareBoard.id);
+            })
+    
+            squareBoard.className = "square";
+            mainBoard.appendChild(squareBoard);
+            makeBoard.innerText = 'Reset Board';
+        }
+    
+}
+// --------------------------------------------------------------
+function gameStart(buttonId){
+    if(mainBoard.hasChildNodes()){
+        // TODO
+        drawMarks(buttonId);
     }
 }
 
+function drawMarks(buttonId){
+    const temp = document.getElementById(`${buttonId}`);
+
+    temp.innerText = "X";
+}
+// --------------------------------------------------------------
 makeBoard.addEventListener("click", () => {
     console.log("Create board is pressed");
     createboard();
